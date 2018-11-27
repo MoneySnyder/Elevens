35581d65cccc12d7555edd606a0a777a31113b82
Edmon Snyder <43817240+MoneySnyder@users.noreply.github.com>
2018-11-27 09:41:03 -0500

    Fixed runtime issues.

--- src/elevens/Elevens.java
+++ src/elevens/Elevens.java
@@ -1,44 +1,25 @@
-/* Program Information
- * 1-15
- * cs.evsnyder
- * Edmon Snyder
- * AP Elevens Lab
- * 11/26/2018
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
-
 package elevens;
 
-// Imports
-import static com.sun.org.apache.xalan.internal.lib.ExsltDynamic.map;
-import java.util.Random;
 import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Hashtable;
-import javax.imageio.ImageIO;
-import static jdk.nashorn.internal.objects.NativeArray.map;
-import static jdk.nashorn.internal.objects.NativeDebug.map;
+import java.util.Random;
 
-/** Credits
+/**
+ *
  * @author cs.evsnyder
  */
 public class Elevens {
-    
-    public static void shuffleDeck(File Deck[]){
-        // Shuffle Based on a Deck of 52 Cards
-        Random x = new Random();
-        for(int i = 0; i < 52; i++){
-            int y = i+x.nextInt(52-i);
-            File temp = Deck[y];
-            Deck[y] = Deck[i];
-            Deck[i] = temp;
-        }
-    }
-    
-    
+
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        // Start Of Card Value Assignment
         Map<String, String> cd = new HashMap<String, String>();
         cd.put("2_of_clubs","2");cd.put("2_of_diamonds","2");cd.put("2_of_hearts","2");cd.put("2_of_spades","2");
         cd.put("3_of_clubs","3");cd.put("3_of_diamonds","3");cd.put("3_of_hearts","3");cd.put("3_of_spades","3");
@@ -56,8 +37,19 @@ public class Elevens {
         // End of Card Value Assignment
         
         File[] Deck = new File("src/cards").listFiles(); 
+        System.out.println("Test: "+cd.get(Deck[0].getName()));
         shuffleDeck(Deck);
     }
     
+    public static void shuffleDeck(File Deck[]){
+        // Shuffle Based on a Deck of 52 Cards
+        Random x = new Random();
+        for(int i = 0; i < 52; i++){
+            int y = i+x.nextInt(52-i);
+            File temp = Deck[y];
+            Deck[y] = Deck[i];
+            Deck[i] = temp;
+        }
+    }
     
 }
